82d74ed4230df0eb3e38a8be6b4415237b63f442
vks4git <vks_m@niuitmo.ru>
2018-04-10 17:49:58 +0300

    Filtration & tag

--- src/System/MQ/Protocol/Internal/Tag.hs
+++ src/System/MQ/Protocol/Internal/Tag.hs
@@ -1,20 +1,50 @@
 module System.MQ.Protocol.Internal.Tag
   (
     messageTag
+  , messageType
+  , messageSpec
+  , messageId
+  , messagePid
+  , messageCreator
   ) where
 
 import           Data.List                         (intercalate)
-import           System.MQ.Protocol.Internal.Types (Message (..), MessageTag)
+import           Data.List.Split                   (splitOn)
+import           System.MQ.Protocol.Internal.Types (Hash, Message (..),
+                                                    MessageTag)
 
 -- | Build a 'MessageTag' for the given message.
 -- It is consists of five fields – message_type, spec, id, pid, creator – separated by ":".
 -- See doc/PROTOCOL.md#Заголовок-сообщения for more information.
 messageTag :: Message -> MessageTag
-messageTag = intercalate ":" . ([messageType, msgSpec, msgId, msgPid, msgCreator] <*>) . pure
+messageTag = intercalate ":" . ([msgType, msgSpec, msgId, msgPid, msgCreator] <*>) . pure
 
 -- | Helper function which returns message type.
-messageType :: Message -> String
-messageType ConfigMessage {} = "config"
-messageType ResultMessage {} = "result"
-messageType ErrorMessage {}  = "error"
-messageType DataMessage {}   = "data"
+msgType :: Message -> String
+msgType ConfigMessage {} = "config"
+msgType ResultMessage {} = "result"
+msgType ErrorMessage {}  = "error"
+msgType DataMessage {}   = "data"
+
+-- | Filtration:
+-- Use System.MQ.Protocol.Internal.Condition
+-- > "foo:bar:baz:ss:er" `matches` (messageType :== "foo" :&& messageId :== "baz")
+-- > True
+-- > "foo:bar:baz:ss:er" `matches` (messageType :== "foo" :&& messageId :== "ba")
+-- > False
+
+messageType :: MessageTag -> String
+messageType = head . splitOn ":"
+
+messageSpec :: MessageTag -> String
+messageSpec = (!! 1) . splitOn ":"
+
+messageId :: MessageTag -> Hash
+messageId = (!! 2) . splitOn ":"
+
+messagePid :: MessageTag -> Hash
+messagePid = (!! 3) . splitOn ":"
+
+messageCreator :: MessageTag -> String
+messageCreator = (!! 4) . splitOn ":"
+
--- src/System/MQ/Protocol/Internal/Types.hs
+++ src/System/MQ/Protocol/Internal/Types.hs
@@ -15,16 +15,20 @@ import           Data.MessagePack.Types.Object ()
 import           GHC.Generics                  (Generic (..))
 
 -- | Represents Unix epoch time in milliseconds.
+--
 type Timestamp  = Int
 
 -- | Represents SHA-1 hash sum.
+--
 type Hash       = String
 
 -- | Message tag is a bytestring with five separated with ':' fields: message type; spec; id; pid; creator.
 -- See doc/PROTOCOL.md for more details.
+--
 type MessageTag = String
 
 -- | Message is the main entity in MQ: various components, controllers and the Scheduler communicate with each other using Messages.
+--
 data Message
   = ConfigMessage { msgId        :: Hash
                   , msgPid       :: Hash
