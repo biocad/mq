2e663d10b3a1568ce4157edf82dadfe298a98896
Bogdan Neterebskii <bog2dan1@gmail.com>
2018-04-10 18:32:13 +0300

    condition

--- src/System/MQ/Protocol.hs
+++ src/System/MQ/Protocol.hs
@@ -4,8 +4,10 @@ module System.MQ.Protocol
   , MessageTag
   , Message (..)
   , messageTag
+  , module System.MQ.Protocol.Internal.Condition
   ) where
 
-import           System.MQ.Protocol.Internal.Tag   (messageTag)
-import           System.MQ.Protocol.Internal.Types (Hash, Message (..),
-                                                    MessageTag, Timestamp)
+import           System.MQ.Protocol.Internal.Condition
+import           System.MQ.Protocol.Internal.Tag       (messageTag)
+import           System.MQ.Protocol.Internal.Types     (Hash, Message (..),
+                                                        MessageTag, Timestamp)
