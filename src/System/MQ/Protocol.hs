c761377aa8b7434ee29d5c864549a6cb6d6fed4c
vks4git <vks_m@niuitmo.ru>
2018-04-10 11:08:05 +0300

    Message and helper types

---
+++ src/System/MQ/Protocol.hs
@@ -0,0 +1,9 @@
+module System.MQ.Protocol
+  ( Timestamp
+  , Hash
+  , MessageTag
+  , Message (..)
+  ) where
+
+import           System.MQ.Protocol.Internal.Types (Hash, Message (..),
+                                                    MessageTag, Timestamp)
