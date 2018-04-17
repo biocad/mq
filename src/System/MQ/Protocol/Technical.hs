c6010b79c3a521cb6c48f3bac7656f29d12fba27
AlexKaneRUS <AlexKaneRUS@gmail.com>
2018-04-17 16:38:31 +0300

    Technical protocol added

--- src/System/MQ/Protocol/Technical.hs
+++ src/System/MQ/Protocol/Technical.hs
@@ -1,1 +1,54 @@
-module System.MQ.Protocol.Technical where
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module System.MQ.Protocol.Technical
+  ( KillConfig (..)
+  , MonitoringConfig (..)
+  , MonitoringResult (..)
+  ) where
+
+import           Data.Aeson                        (FromJSON (..), ToJSON (..),
+                                                    genericParseJSON,
+                                                    genericToJSON, object,
+                                                    withObject, (.:), (.=))
+import           Data.Aeson.Casing                 (aesonPrefix, snakeCase)
+import qualified Data.ByteString.Char8             as BSLC8 (pack, unpack)
+import           Data.MessagePack                  (MessagePack)
+import           GHC.Generics                      (Generic)
+import           System.MQ.Protocol.Internal.Types (Hash, Timestamp)
+
+-- | Configuration for kill task
+--
+data KillConfig = KillConfig { killTaskId :: Hash
+                             } deriving (Eq, Show, Generic, MessagePack)
+
+instance ToJSON KillConfig where
+  toJSON p = object [ "task_id" .= (BSLC8.unpack $ killTaskId p) ]
+instance FromJSON KillConfig where
+  parseJSON = withObject "Kill Config" $ \o -> KillConfig . BSLC8.pack <$> o .: "task_id"
+
+-- | Configuration of monitoring task
+--
+data MonitoringConfig = MonitoringConfig { syncTime :: Timestamp
+                                         } deriving (Eq, Show, Generic, MessagePack)
+
+instance ToJSON MonitoringConfig where
+  toJSON = genericToJSON $ aesonPrefix snakeCase
+instance FromJSON MonitoringConfig where
+  parseJSON = genericParseJSON $ aesonPrefix snakeCase
+
+-- | Format of data that is produced as result of monitoring task
+--
+data MonitoringResult = MonitoringResult { rSyncTime  :: Timestamp
+                                         , rName      :: String
+                                         , rHost      :: String
+                                         , rIsRunning :: Bool
+                                         , rMessage   :: String
+                                         } deriving (Eq, Show, Generic, MessagePack)
+
+instance ToJSON MonitoringResult where
+  toJSON = genericToJSON $ aesonPrefix snakeCase
+instance FromJSON MonitoringResult where
+  parseJSON = genericParseJSON $ aesonPrefix snakeCase
