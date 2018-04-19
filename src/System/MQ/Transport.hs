23dcdb92f98d71c8fd453ca5a59a02d553e61b34
Bogdan Neterebskii <bog2dan1@gmail.com>
2018-04-19 18:16:46 +0300

    push and pub feature

--- src/System/MQ/Transport.hs
+++ src/System/MQ/Transport.hs
@@ -9,7 +9,8 @@ module System.MQ.Transport
 
 import           System.MQ.Encoding.MessagePack         (pack, unpackM)
 import           System.MQ.Monad                        (MQMonad)
-import           System.MQ.Protocol                     (Message, MessageTag)
+import           System.MQ.Protocol                     (Message, MessageTag,
+                                                         messageTag)
 import           System.MQ.Protocol.Internal.Instances  ()
 import qualified System.MQ.Transport.ByteString         as TBS (pub, pull, push,
                                                                 sub)
@@ -17,9 +18,11 @@ import           System.MQ.Transport.Internal.Instances ()
 import           System.MQ.Transport.Internal.Types
 
 -- | Pushes @(tag, content)@ to the 'PushChannel'.
+-- @tag::'MessageTag'@ is generated automatically from @content@.
 --
-push :: PushChannel -> (MessageTag, Message) -> MQMonad ()
-push channel (tag, content) = TBS.push channel (pack tag, pack content)
+push :: PushChannel -> Message -> MQMonad ()
+push channel content = let tag' = pack . messageTag $ content
+                       in TBS.push channel (tag', pack content)
 
 -- | Pulls @(tag, content)@ from the 'PullChannel'.
 --
@@ -31,9 +34,11 @@ pull channel = do
   pure (utag, ucontent)
 
 -- | Publishes @(tag, content)@ to the 'PubChannel'.
+-- @tag::'MessageTag'@ is generated automatically from @content@.
 --
-pub :: PubChannel -> (MessageTag, Message) -> MQMonad ()
-pub channel (tag, content) = TBS.pub channel (pack tag, pack content)
+pub :: PubChannel -> Message -> MQMonad ()
+pub channel content = let tag' = pack . messageTag $ content
+                      in TBS.pub channel (tag', pack content)
 
 -- | Subscribes and gets @(tag, content)@ from the 'SubChannel'.
 --
